100c7d46bd1cca1696d4f4b9019ff595a47877cc
Mauro  Navarro <mauronav006gmail.com>
2019-09-23 03:00:49 -0600

    Crear Compuertas
    Se modificaron las imágenes de las compuertas y ahora son botones. Se pueden colocar compuertas en el sector de diseño.

--- src/Interfaz/PartsWn.java
+++ src/Interfaz/PartsWn.java
@@ -7,11 +7,13 @@ import Logica.LogicGate;
 import javafx.geometry.Insets;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
-
 
 
 
@@ -23,11 +25,13 @@ public class PartsWn {
     Image notImage = new Image("images/not.png");
     Image xnorImage = new Image("images/xnor.png");
     Image xorImage = new Image("images/xor.png");
+    double mousePosX;
+    double mousePosY;
 
     LinkedLists lista1=new LinkedLists();
 
 
-    public VBox addVBox() {
+    public VBox addVBox(int width, int height, int spacing, String color, VBox wind) {
         this.lista1.insert(xorImage);
         this.lista1.insert(xnorImage);
         this.lista1.insert(notImage);
@@ -35,25 +39,49 @@ public class PartsWn {
         this.lista1.insert(nandImage);
         this.lista1.insert(orImage);
         this.lista1.insert(andImage);
-        System.out.println("Si lo ejecuta");
+        System.out.println("Si lo ejecuta" + "-fx-background-color: #336699;");
 
         VBox vbox = new VBox();
-        vbox.setPrefSize(200,700);
+        vbox.setPrefSize(width, height);
         vbox.setPadding(new Insets(15, 12, 15, 12));
-        vbox.setSpacing(10);
-        vbox.setStyle("-fx-background-color: #336699;");
+        vbox.setSpacing(spacing);
+        vbox.setStyle(color);
 
-        for (int i= 0; i != 7; i++ ){
+        for (int i = 0; i != 7; i++) {
 
-           ImageView iv = new ImageView();
-           Image imagen = (Image) this.lista1.get(i);
-           iv.setImage(imagen);
-           vbox.getChildren().add(iv);
+            ImageView iv = new ImageView();
+            Image imagen = (Image) this.lista1.get(i);
+            iv.setImage(imagen);
+
+            Button button = new Button();
+            button.setGraphic(iv);
+            button.setPrefSize(150,80);
+            button.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    button.setOnMouseMoved(new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent event) {
+                            mousePosX = event.getX();
+                            System.out.println(mousePosX);
+                            mousePosY = event.getY();
+                            System.out.println(mousePosY);
+
+
+                        }
+                    });
+
+                }
+            });
+
+            vbox.getChildren().add(button);
+
+
 
         }
 
         Button readyBtn = new Button("Test Circuit");
-        readyBtn.setPrefSize(150,20);
+        readyBtn.setPrefSize(150, 20);
 
         readyBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -71,8 +99,6 @@ public class PartsWn {
 
             }
         });
-
-
 
 
         vbox.getChildren().add(readyBtn);
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,16 @@ import Estructuras.LinkedLists;
 import Interfaz.DragAndDrop;
 import Interfaz.PartsWn;
 
+import Logica.LogicGate;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
@@ -15,23 +19,28 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Main extends Application {
     Stage window;
     BorderPane mainPane;
     VBox source;
-    ImageView imageView;
-    VBox center;
+    Pane center;
     Image andImage = new Image("images/and.png");
+    Image orImage = new Image("images/or.png");
+    Image nandImage = new Image("images/nand.png");
+    Image norImage = new Image("images/nor.png");
+    Image notImage = new Image("images/not.png");
+    Image xnorImage = new Image("images/xnor.png");
+    Image xorImage = new Image("images/xor.png");
+    double mousePosX;
+    double mousePosY;
+
+    LinkedLists lista1=new LinkedLists();
 
 
     @Override
@@ -46,12 +55,15 @@ public class Main extends Application {
         mainPane = new BorderPane();
         Scene mainScene = new Scene (mainPane,800,700);
 
-        PartsWn rightSide = new PartsWn();
-        mainPane.setRight(rightSide.addVBox());
 
+        source = addVBox(200,700,8, "-fx-background-color:#A6DF34");
+        mainPane.setRight(source);
 
-        center = new VBox();
+        center = new Pane();
         mainPane.setCenter(center);
+
+
+
 
         // Crear menú en la parte superior
         Menu infoMenu = new Menu("Info");
@@ -65,6 +77,141 @@ public class Main extends Application {
         window.show();
 
     }
+
+    public VBox addVBox(int width, int height, int spacing, String color) {
+        this.lista1.insert(xorImage);
+        this.lista1.insert(xnorImage);
+        this.lista1.insert(notImage);
+        this.lista1.insert(norImage);
+        this.lista1.insert(nandImage);
+        this.lista1.insert(orImage);
+        this.lista1.insert(andImage);
+        System.out.println("Si lo ejecuta" + "-fx-background-color: #336699;");
+
+        VBox vbox = new VBox();
+        vbox.setPrefSize(width, height);
+        vbox.setPadding(new Insets(15, 12, 15, 12));
+        vbox.setSpacing(spacing);
+        vbox.setStyle(color);
+
+        for (int i = 0; i != 7; i++) {
+
+            ImageView iv = new ImageView();
+            Image imagen = (Image) this.lista1.get(i);
+            iv.setImage(imagen);
+
+            Button button = new Button();
+            button.setGraphic(iv);
+            button.setPrefSize(150,80);
+
+            button.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    System.out.println("A gate has been clicked !!");
+                    center.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent event) {
+                            mousePosX = event.getX();
+                            System.out.println("Click X on: " + mousePosX);
+                            mousePosY = event.getY();
+                            System.out.println("Click Y on: " + mousePosY);
+
+                           double selectedX = valid_X(mousePosX);
+                           double selectedY = valid_Y(mousePosY);
+
+                            System.out.println("SelecX: "+selectedX);
+                            System.out.println("SelecY: "+selectedY);
+
+                           if(selectedX < 577 && selectedY < 635) {
+                               placeImage(imagen, selectedX, selectedY);
+                           }
+                           else{
+                               System.out.println("Fuera del rango");
+                           }
+
+
+
+                        }
+                    });
+
+                }
+            });
+
+
+
+            vbox.getChildren().add(button);
+
+
+
+        }
+
+        Button readyBtn = new Button("Test Circuit");
+        readyBtn.setPrefSize(150, 20);
+
+        readyBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                LogicGate comp0 = new LogicGate();
+                LogicGate comp1 = new LogicGate();
+                LogicGate comp2 = new LogicGate();
+                LogicGate comp3 = new LogicGate();
+
+                comp1.connection(comp2);
+                comp3.connection(comp2);
+
+                LogicGate a = (LogicGate) comp2.connections.get(0);
+                System.out.println(a.gateID);
+
+            }
+        });
+
+
+        vbox.getChildren().add(readyBtn);
+
+        return vbox;
+    }
+
+   public ImageView placeImage (Image image, double coordX, double coordY){
+       ImageView newIV = new ImageView(image);
+       newIV.setX(coordX);
+       newIV.setY(coordY);
+
+       center.getChildren().add(newIV);
+       return newIV;
+   }
+
+   public double valid_X (double value){
+        if (value <= 577){
+            if(value <= 410){
+                return value;
+
+            }
+            else{
+                double newValue = 410;
+                return newValue;
+            }
+        }
+
+        value++;
+        return value;
+
+   }
+
+   public double valid_Y (double value){
+       if (value <= 635){
+           if(value <= 550){
+               return value;
+
+           }
+           else{
+               double newValue = 550;
+               return newValue;
+           }
+       }
+
+       value ++;
+       return value;
+   }
 
 
     public static void main (String[] args) {
